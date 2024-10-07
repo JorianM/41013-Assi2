7893e67ca10f4187356ed730bd4684e186e25f9e
jorianmitchell <jorianmitchell@Jorians-Mac-Air.local>
2024-10-07 17:03:50 +1100

    More structuring and planning out.

---
+++ @Main/PlotDrinks.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = PlotDrinks(inputArg1,inputArg2)
+%PLOTDRINKS Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
